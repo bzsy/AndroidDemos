0eb82fb4991d58121b362ab59b0a967aafadc89e
bzsy <bzsy@icloud.com>
2014-10-07 12:33:35 +0800

    Add 主界面底部导航栏; Update xml

---
+++ QQDemo/src/com/bzsy/qqdemo/FragmentContact.java
@@ -0,0 +1,22 @@
+package com.bzsy.qqdemo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class FragmentContact extends Fragment {
+	private View v;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		v = inflater
+				.inflate(R.layout.layout_fragment_example, container, false);
+		TextView textView = (TextView) v.findViewById(R.id.textView);
+		textView.setText("Fragment Contact");
+		return v;
+	}
+}
